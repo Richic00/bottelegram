f6c57d993ef5bdc4c98facaa55580cabb328999b
Richic00 <45816450+Richic00@users.noreply.github.com>
2018-12-12 10:38:34 +0000

    Update register.php

--- register.php
+++ register.php
@@ -3,12 +3,12 @@
 		// PARAMETRI DA MODIFICARE
 		//inserite vostro indirizzo su heroku 
 		//https://username.herokuapp.com/execute.php
-		$WEBHOOK_URL = 'https://trap-app.herokuapp.com/';
+		$WEBHOOK_URL = 'https://trap-app.herokuapp.com/execute.php';
 		//inserite il vostro token che vi ha fornito l'utente telegram @botfather
 		$BOT_TOKEN = '724039027:AAEl6pJAAK7h2Mj-OUQnMgia1tvH6uCPvIQ';
 
 		// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
-		$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+		$API_URL = 'https://api.telegram.org/bot/' . $BOT_TOKEN .'/';
 		$method = 'setWebhook';
 		$parameters = array('url' => $WEBHOOK_URL);
 		$url = $API_URL . $method. '?' . http_build_query($parameters);
