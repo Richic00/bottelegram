2eb179b40fc345c5027cadf6ad848e10a9abd451
Richic00 <45816450+Richic00@users.noreply.github.com>
2019-01-16 11:01:40 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -89,8 +89,8 @@
 		}
 
 
-		if($text =="pacchi"){
-			sendAudio($chatId, "BucarestTony.mp3",false, "Il mio audio, $api);
+		if($text =="BucarestTony"){
+			sendAudio($chatId, "BucarestTony.mp3",false, "BucarestTony", $api);
 		}
 
 
